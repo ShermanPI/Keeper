9f1a4026078e6edf9f3a5375f692f77bbdb48d68
ShermanPI <shermanperez0@gmail.com>
2023-09-09 12:51:54 -0400

    :construction: build: reset for styles and file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Index from './components'
+import Home from './routes/Home'
 
 function App () {
   return (
     <>
       <Routes>
         <Route path='/' element={<Index />} />
+        <Route path='/home' element={<Home />} />
       </Routes>
     </>
   )
