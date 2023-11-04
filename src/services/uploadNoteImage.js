2e9df5394a91f793aae76209d348a32efe47ddcd
Sherman Perez <shermanperez0@gmail.com>
2023-11-04 19:41:37 -0400

    the user can upload the images

--- src/services/uploadNoteImage.js
+++ src/services/uploadNoteImage.js
@@ -3,11 +3,11 @@ import { supabase } from './clients/supabaseClient'
 export default async function uploadNoteImage ({ file }) {
   try {
     console.log(file)
-    const { data, error } = await supabase
+    const { data } = await supabase
       .storage
       .from('note_images')
       .upload(`public/note.${file.type.split('/')[1]}`, file)
-    console.log(`note.${file.type.split('/')[1]}`, error, data)
+    console.log(`note.${file.type.split('/')[1]}`, data, '👽👽👽')
   } catch (error) {
     console.error('An error ocurred while attempting uploading the image', error.message)
   }
