5e3b9447bbd48e3b444e0a831483549148ddd9e1
Sherman Perez <shermanperez0@gmail.com>
2023-11-21 22:12:16 -0400

    the user can see the note tags

--- src/components/TagManager/TagManager.jsx
+++ src/components/TagManager/TagManager.jsx
@@ -1,24 +1,59 @@
-import { useRef, useState, useContext } from 'react'
+import { useRef, useState, useContext, useEffect, useMemo } from 'react'
 import { AddIcon, MagnifyingGlasses } from '../Note/assets/images/Icons'
 import './assets/style/TagManager.css'
 import createNewNoteTag from '../../services/note/createNewNoteTag'
 import { session } from '../../context/contextLogin'
 import { tagContext } from '../../context/tagContext'
+import { tagNote } from '../../services/note/tagNote'
+import { getNoteTags } from '../../services/note/getNoteTags'
 
-export default function TagManager ({ isVisible }) {
+export default function TagManager ({ isVisible, noteId }) {
   const tagInputRef = useRef()
   const [query, setQuery] = useState('')
   const { loggedUser } = useContext(session)
-  const { tags, addTag } = useContext(tagContext)
+  const { tags: generalTags, addTag } = useContext(tagContext)
+  const [selectedTags, setSelectedTags] = useState([])
 
-  const handleChange = (e) => {
+  useEffect(() => {
+    if (noteId) {
+      (async () => {
+        const newSelectedTags = await getNoteTags({ noteId })
+        setSelectedTags(newSelectedTags)
+      })()
+    }
+  }, [noteId])
+
+  const handleCheckbox = ({ event, noteId, tagId }) => {
+    event.target.checked = !event.target.checked
+    tagNote({ noteId, tagId })
+  }
+
+  const generalTagsAndSelectedTags = useMemo(() => {
+    return generalTags.map((el) => {
+      console.log(selectedTags.indexOf((el.id)) !== -1, el.id)
+      return (
+        <li key={el.id}>
+          <label>
+            <input
+              type='checkbox' name='checkbox' checked={(selectedTags.indexOf((el.id)) !== -1)} onChange={(event) => {
+                handleCheckbox({ event, noteId, tagId: el.id })
+              }}
+            />
+            {el.name}
+          </label>
+        </li>
+      )
+    })
+  }, [selectedTags, generalTags])
+
+  const handleInputChange = (e) => {
     if (e.target.value.length <= 50) {
       setQuery(e.target.value)
     }
   }
 
   const handleCreateNote = async () => {
-    if (query.length > 0 && !tags.find(({ name }) => name === query)) {
+    if (query.length > 0 && !generalTags.find(({ name }) => name === query)) {
       const newTag = await createNewNoteTag({ userId: loggedUser.id, name: query })
       addTag({ newTag })
       setQuery('')
@@ -29,18 +64,12 @@ export default function TagManager ({ isVisible }) {
     <div className={`card-tags-container ${!isVisible && 'hidden-note-tag'}`} onClick={(e) => e.stopPropagation()}>
       <h3>Tag Note</h3>
       <label className='search-tag-container' ref={tagInputRef}>
-        <input type='text' className='search-label-input' placeholder='Enter label name' value={query} onChange={handleChange} />
+        <input type='text' className='search-label-input' placeholder='Enter label name' value={query} onChange={handleInputChange} />
         <MagnifyingGlasses />
       </label>
 
       <ul className='note-tag-list'>
-        {tags.map((el) => {
-          return (
-            <li key={el.id}>
-              <label><input type='checkbox' name='checkbox' value='value' defaultChecked />{el.name}</label>
-            </li>
-          )
-        })}
+        {generalTagsAndSelectedTags}
       </ul>
       <button className='add-label-btn' onClick={handleCreateNote}>
         <AddIcon />
